4d732774f2468a646de34f84ad882e1bee2e441f
atheeq2003 <atheequrrahaman8@gmail.com>
2020-12-15 16:53:15 +0530

    344A-Magnets

---
+++ EasyProgs_800/344A_Magnets.c
@@ -0,0 +1,16 @@
+// A. Magnets
+#include <stdio.h>
+int main()
+{
+    int i, n, a = 0, r=0;
+    scanf("%d", &i);
+
+    while (i--)
+    {
+        scanf("%d", &n);
+        if (n != a)
+            r++;
+        a = n;
+    }
+    printf("%d", r);
+}
